801b24ec7fbecb6737f01e193330b26a21c5ab37
David C McGeary <43143579+dmcgeary76@users.noreply.github.com>
2018-10-18 15:13:32 -0500

    Create get_student_list.py
    Gets a list of students enrolled in a given course with a specific course id.  I just hardcoded the id to make life easier, but if you wanted to do something similar for all courses, you could just create a separate list with the appropriate scope in another file and then call that function and run the get-students function against the procured numbers list.
    
    I'm certain that there is a better way to do all of this.  I am just struggling through as I go.

---
+++ get_student_list.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/classroom.rosters.readonly'
+course_id = '16954164213'
+
+def get_students(temp_id):
+    '''
+    Get a list of the students (and their corresponding ids) from the target course roster.
+    :param temp_id:
+    :return:
+    '''
+    store = file.Storage('token2.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('classroom', 'v1', http=creds.authorize(Http()))
+
+    results = service.courses().students().list(courseId=temp_id).execute()
+    student_list = results.get('students', [])
+
+    student_master = [[] for i in range(len(student_list))]
+
+    count = 0
+    for student in student_list:
+        student_master[count].append(student['userId'])
+        student_master[count].append(student['profile']['name']['fullName'])
+        count += 1
+
+    print(student_master)
+    return student_master
+
+if __name__ == '__main__':
+    get_students(course_id)
+
+
+
