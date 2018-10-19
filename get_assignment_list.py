58a0bf1765b64fb28b3fc2f87465ebd75aa4c7dd
David C McGeary <43143579+dmcgeary76@users.noreply.github.com>
2018-10-19 12:44:28 -0500

    Create get_assignment_list.py
    Pulls a list of all assignments in a course - includes the title of the assignment and the unique id for the assignment.  The data from this list will be used to pull grade data.

---
+++ get_assignment_list.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/classroom.coursework.students.readonly'
+course_id = '16954164213'
+
+def get_assignments(temp_id):
+    '''
+    Get a list of the assignments (and their ids) from the target course roster.
+    :param temp_id:
+    :return:
+    '''
+    store = file.Storage('token3.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('classroom', 'v1', http=creds.authorize(Http()))
+
+    results = service.courses().courseWork().list(courseId=temp_id).execute()
+    activity_list = results.get('courseWork', [])
+
+    activity_master = [[] for i in range(len(activity_list))]
+
+    count = 0
+    for activity in activity_list:
+        activity_master[count].append(activity['id'])
+        activity_master[count].append(activity['title'])
+        count += 1
+
+    return activity_master
+
+if __name__ == '__main__':
+    get_assignments(course_id)
