4e100e726d244844f8fa10d3607f8c4b65bea986
David C McGeary <43143579+dmcgeary76@users.noreply.github.com>
2018-11-20 12:09:38 -0600

    Update get_course_list.py
    Make determination of course id changes to make it easier

--- get_course_list.py
+++ get_course_list.py
@@ -1,9 +1,3 @@
-'''
-On the off chance that someone has multiple courses that they would like to process a CSV for, I have included a short
-function that will return a course list with the name and id of each course.  This can be used to create a master script
-would roll through all of the available courses and create new csvs for each one.
-'''
-
 from __future__ import print_function
 from googleapiclient.discovery import build
 from httplib2 import Http
@@ -11,9 +5,8 @@ from oauth2client import file, client, tools
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/classroom.courses.readonly'
-course_id = courseId
 
-def get_course_list():
+def get_courses():
     '''
     Get a list of the students (and their corresponding ids) from the target course roster.
     :param temp_id:
@@ -29,7 +22,14 @@ def get_course_list():
     results = service.courses().list().execute()
     course_list = results.get('courses', [])
 
-    return course_list
+    simple_list = [[] for course in course_list]
+
+    count = 0
+    for course in course_list:
+        simple_list[count].append(course['id'])
+        simple_list[count].append(course['name'])
+        count += 1
+    return simple_list
 
 if __name__ == '__main__':
-    get_course_list()
+    print(get_courses())
