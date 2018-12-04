02f9b03ec3618852730ed984910503e55c0b4912
David C McGeary <43143579+dmcgeary76@users.noreply.github.com>
2018-12-04 09:46:36 -0600

    Update get_grade_data.py
    Added a way to write new grade entry data into db table.

--- get_grade_data.py
+++ get_grade_data.py
@@ -6,23 +6,27 @@ from oauth2client import file, client, tools
 # Import a student_list function so that we can identify course participants
 from get_student_list import get_students
 from get_assignment_list import get_assignments
+from get_course_list import get_courses
+from mysql_demo import insert_grade, get_course_id, get_assign_id, get_student_id
+from get_config import get_config_vals
+
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/classroom.coursework.students.readonly'
-course_id = courseId
-
-student_key = get_students(course_id)
-assignment_key = get_assignments(course_id)
+course_id = '20106357846'
 
 
 def get_grades(temp_id, tempwork_id):
     '''
-    get the grade data from each submitted assignment.  The only information of value here is the id value for the
-    student, the link to the student's grade file, and the grade or grade status as a string
+    get the grade data from each submitted assignment.
+    The only information of value here is the id value for the
+    student, the link to the student's grade file, and the grade or
+    grade status as a string
     :param temp_id:
     :param tempwork_id:
     :return:
     '''
+    student_key = get_students(temp_id)
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
@@ -33,36 +37,18 @@ def get_grades(temp_id, tempwork_id):
     results = service.courses().courseWork().studentSubmissions().list(courseId=temp_id, courseWorkId=tempwork_id).execute()
     submission_list = results.get('studentSubmissions', [])
 
-    submission_master = [[] for i in range(len(submission_list))]
-
-    count = 0
+    # This needs to happen in all get_scripts.  Uses grade submission data to write new submissions to db.
     for submission in submission_list:
-        submission_master[count].append(submission['userId'])
-        submission_master[count].append(submission['alternateLink'])
-        if submission['state'] == 'RETURNED':
-            submission_master[count].append(submission['assignedGrade'])
-        elif submission['state'] == 'TURNED_IN':
-            submission_master[count].append('Assignment is being graded.')
-        else:
-            submission_master[count].append('Assignment not yet completed')
-        count += 1
-    return submission_master
+        insert_grade(submission)
 
 
-def get_progress_list(temp_assignment_key, temp_course_id):
-    '''
-    The progress_list is meant to pull all grade data into one large list.  This is necessary for the creation of the
-    progress report pdf document and the email that is automatically sent out.
-    :param temp_assignment_key:
-    :param temp_course_id:
-    :return:
-    '''
-    temp_progress_list = [[] for i in range(len(temp_assignment_key))]
-    count = 0
-    for assignment in assignment_key:
-        temp_progress_list[count].append(get_grades(temp_course_id, assignment[0]))
-        count += 1
-    return temp_progress_list
+def main():
+    courses = get_courses()
+    for course in courses:
+        assignments = get_assignments(course[0])
+        for assignment in assignments:
+            get_grades(course[0],assignment[0])
+
 
 if __name__ == '__main__':
-    get_progress_list(assignment_key, course_id)
+    main()
